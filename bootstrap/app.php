71548319e75473bf4cd3417ceafddad306d842cc
omkar-chanchle <omkarchanchle.dev@gmail.com>
2024-12-30 22:53:16 +0530

    Commented CSP middleware
    Reason
    > Bug : conflict between style directive's self and unsafe-line key

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append([
             ProtectAgainstSpam::class,
-            AddCspHeaders::class,
+            // AddCspHeaders::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
